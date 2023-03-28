dd4b1181bd291abcd4867ba34810ab5671009d4c
Larissa Albuquerque <105246799+larialbu@users.noreply.github.com>
2023-03-28 15:46:05 -0300

    Update Contador.java

--- atv1/Contador.java
+++ atv1/Contador.java
@@ -1,18 +1,18 @@
 public class Contador {
     private int valor;
-    
-    public Contador(int v) {
-        this.valor = v;
+
+    public Contador() {
+        this.valor = 0;
     }
-    
+
     public void zerar() {
         this.valor = 0;
     }
-    
+
     public void incrementar() {
         this.valor++;
     }
-    
+
     public int getValor() {
         return this.valor;
     }
